2b5ad2d2a10492ef1f992fcd485d243ba4d00a67
Dominik Deutsch <dominik.deutsch@panagenda.com>
2020-07-10 18:08:28 +0200

    pass server / db as programm arguments

--- src/main/java/com/github/floushee/notescalendarsync/App.java
+++ src/main/java/com/github/floushee/notescalendarsync/App.java
@@ -7,9 +7,20 @@ import lotus.domino.NotesThread;
 
 import java.util.List;
 
+import static java.lang.System.exit;
+
 final class App {
 
     public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("Usage: app.jar <server>, <filepath>");
+            exit(1);
+        }
+
+        String server = args[0];
+        String filePath = args[1];
+
         boolean notesInitialized = false;
         try {
 
@@ -20,21 +31,23 @@ final class App {
 
             NotesGC.runWithAutoGC(() -> {
                 System.out.println("Username of Notes ID: "+IDUtils.getIdUsername());
-                run();
+                run(server, filePath);
                 return null;
             });
         } catch (Exception e) {
             e.printStackTrace();
+            exit(1);
         } finally {
             NotesThread.stermThread();
             if (notesInitialized) {
                 NotesInitUtils.notesTerm();
             }
         }
+        exit(0);
     }
 
-    private static void run() {
-        List<CalendarEntry> entries = NotesCalendarReader.readNotesCalendarEntries("", "");
+    private static void run(String server, String filePath) {
+        List<CalendarEntry> entries = NotesCalendarReader.readNotesCalendarEntries(server, filePath);
         entries.forEach(entry -> {
             System.out.println(entry);
         });
